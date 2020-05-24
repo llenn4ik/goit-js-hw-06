eb1d6f536c69fd3779ee6ddae3af7105a1b92871
llenn4ik <57867092+llenn4ik@users.noreply.github.com>
2020-05-24 20:19:40 +0300

    correct t-3 & t-4

--- task-03.js
+++ task-03.js
@@ -1,6 +1,9 @@
 'use strict';
 import users from './users.js';
 
-const getUsersWithGender = (users, gender) => users.filter(user=>user.gender=== gender)
+const getUsersWithGender = (users, gender) =>
+ users
+ .filter(user=>user.gender=== gender)
+ .map(({name}) => name);
   
   console.log(getUsersWithGender(users, 'male'));
--- task-04.js
+++ task-04.js
@@ -1,6 +1,5 @@
 'use strict';
 import users from './users.js';
-const getInactiveUsers = users =>users.filter(user=>user.isActive=== false)
-  
+const getInactiveUsers = users =>users.filter( ({isActive})=>!isActive) 
   
   console.log(getInactiveUsers(users)); 
